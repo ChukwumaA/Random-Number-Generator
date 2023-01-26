b09e7ebdcd8d764457d944ce53be9629718a449c
Chukwuma Akunyili <chukwuma.akunyili.233279@unn.edu.ng>
2023-01-26 11:57:31 +0100

    add: included logic. to execute use dotnet run.

---
+++ RandomNumberGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿var rand = new Random();
+
+var listNumbers = new List<int>();
+var number = 0;
+
+    do {
+        number = rand.Next(1, 29);
+    } while (listNumbers.Contains(number));
+    listNumbers.Add(number);
+
+Console.WriteLine(number);
+    
+
+
